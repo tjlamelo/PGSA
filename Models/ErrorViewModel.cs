7393345f0026b7c9c8982365cee20c3e1d3f9835
TJ <132734178+tjlamelo@users.noreply.github.com>
2025-11-22 15:52:41 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PGSA_Licence3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
